ed997ee92081c7e81c69806e9720f4d99a6ea0e6
David <daviddiezalvarez@gmail.com>
2023-06-07 13:10:30 +0200

    detalle de carta actualizado

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -1,4 +1,7 @@
 const User = require('../models/card.model')
+const Card = require('../models/card.model');
+const axios =require ('axios')
+
 
 //Lógica para traer el .json con la data de las cartas y parsearlo
 // const path = require('path');
@@ -28,4 +31,49 @@ const createAllCardsSummary = (req, res) => {
     })    
 }
 
-module.exports = { createAllCardsSummary }
+const getCardDetail = async function (req, res, next) {
+    const cardId = req.params.cardId; // Obtener el valor del parámetro de ruta ":cardId"
+    try {
+      const card = await Card.findById(cardId); // Buscar la carta por el objectID de mongo
+      if (!card) {
+        // Si no se encuentra la carta, devolver una respuesta de error
+        return res.status(404).json({ error: 'Carta no encontrada' });
+      }      
+      // Obtener el valor de id_scryfall de la carta encontrada
+      const idScryfall = card.id_scryfall;
+      // Obtenemos la carta con ese ID en la API de scryfall con todo su contenido
+      const { data } = await axios.get(`https://api.scryfall.com/cards/${idScryfall}`);
+      // Extraemos los campos que vamos a usar en la vista de carta
+      const {name, oracle_text, set_name, rarity, colors, type_line, image_uris, legalities} = data;
+      // Construimos el objeto de respuesta con esos campos
+      const cardDetail ={
+        name,
+        oracle_text,
+        set_name,
+        rarity,
+        colors,
+        type_line,
+        image_uris,
+        legalities,
+      };
+      
+      console.log('Detalles de la carta:', cardDetail);
+      res.status(200).json(cardDetail);
+
+      // Construir el objeto de respuesta con el id_scryfall de la carta
+      //const cardDetail = {
+        //id_scryfall: idScryfall, 
+        // Agregar más propiedades de la carta según tus necesidades
+      //};
+  
+      //res.status(200).json(cardDetail);
+      //console.log('Detalles de la carta:', cardDetail);
+    } catch (error) {
+      // Manejar errores de base de datos u otros errores
+      console.error('Error al buscar los detalles de la carta:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+};
+
+module.exports = { createAllCardsSummary, getCardDetail }
+
--- routes/cards/index.js
+++ routes/cards/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const cardsRouter = express.Router();
-const { createAllCardsSummary } = require('../../controllers/cardController')
+const { createAllCardsSummary, getCardDetail } = require('../../controllers/cardController')
 const Card = require('../../models/card.model');
-
 
 
 cardsRouter.post('/addAllCards', createAllCardsSummary);
@@ -17,7 +16,8 @@ cardsRouter.get('/random', async function (req, res, next) {
     const count = parseInt(req.query.count) || 1; // Obtener el valor del parámetro count, si no se proporciona, se asume 1
     const randomCards = await Card.aggregate([{ $sample: { size: count } }]);
     
-    const cardData = randomCards.map(({ name, image_uris }) => ({
+    const cardData = randomCards.map(({ _id, name, image_uris }) => ({
+      _id: _id.toString(),
       name,
       normalImageUrl: image_uris.normal
     }));
@@ -25,8 +25,8 @@ cardsRouter.get('/random', async function (req, res, next) {
     res.status(200).json(cardData);
     console.log('cardData es:', cardData);
   });
-
-
+  
+cardsRouter.get('/:cardId', getCardDetail)
 
 
 module.exports = cardsRouter;
