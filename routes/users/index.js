94c81065cb2e9ddc70948cba197dd3e471d5f418
crobledor <crobledoramos@gmail.com>
2023-06-21 12:07:43 +0200

    modify user details and password controllers + token validation middleware

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, loginUser, getProfile, modifyUser } = require('../../controllers/userController')
+const { createUser, loginUser, getProfile, modifyUser, checkPsswd, modifyPsswd } = require('../../controllers/userController')
+const { tokenValidator } = require('../../middlewares/tokenValidator')
 
 /* GET users listing. */
 router.post('/register', createUser);
 
 router.post('/login', loginUser)
 
-router.get('/profile', getProfile)
+router.get('/profile', tokenValidator, getProfile)
 
-router.put('/profile', modifyUser)
+router.put('/profile/modify_details', tokenValidator, modifyUser)
+
+router.put('/profile/check_psswd', tokenValidator, checkPsswd)
+
+router.put('/profile/modify_psswd', tokenValidator, modifyPsswd)
+
+
+
+
 
 module.exports = router;
